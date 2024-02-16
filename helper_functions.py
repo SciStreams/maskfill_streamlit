8aaaec6e4abfbf267932dd63e647ab930931934b
Robert Dzudzar <33647339+rdzudzar@users.noreply.github.com>
2024-02-16 19:05:39 +1100

    Initial

---
+++ helper_functions.py
@@ -0,0 +1,367 @@
+import streamlit as st
+import matplotlib.pyplot as plt 
+import numpy as np 
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+from astropy.io import fits 
+from maskfill import maskfill 
+
+
+@st.cache_data
+def import_m51_sample():
+
+    image_clean = fits.getdata('./example_m51/m51_org.fits')
+    image_cr = fits.getdata('./example_m51/m51_with_cosmicrays.fits')
+    mask = fits.getdata('./example_m51/m51_mask.fits')
+
+    return image_clean, image_cr, mask
+
+
+@st.cache_data
+def run_mfill(image_clean, image_cr, mask):
+
+    mfill_smooth, mfill = maskfill(image_cr, mask)
+
+    return mfill_smooth, mfill
+
+
+def show_data(image_clean, image_cr, mask):
+
+
+    # Create subplots
+    fig = make_subplots(rows=1, cols=6,
+                        subplot_titles=("Clean", "Cosmic Rays", "Mask"),
+                        specs=[[{}, None, {}, None, {}, None]],
+                        shared_yaxes=True)
+
+    # Range
+    vmin,vmax=np.percentile(image_clean,[1,99])
+
+
+    fig.add_trace(go.Heatmap(
+        z=image_clean,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.26, title='Clean', thickness=15)), row=1, col=1
+    )
+
+    fig.add_trace(go.Heatmap(
+        z=image_cr,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.61, title='Clean', thickness=15)), row=1, col=3
+    )
+
+
+    fig.add_trace(go.Heatmap(
+        z=mask,
+        colorscale='gray_r',
+        zmin=0.1,
+        zmax=0.9,
+        colorbar=dict(x=0.96, title='Clean', thickness=15)), row=1, col=5
+    )
+
+    # Update layout
+    fig.update_layout(
+        title_text='Example M51',
+        title_x=0.4,
+        title_y=1,
+        height=250,
+        margin=dict(l=0, r=0, b=40, t=45),
+        xaxis=dict(domain=[0, 0.27]),
+        xaxis2=dict(domain=[0.35, 0.62]),
+        xaxis3=dict(domain=[0.7, 0.97])
+    )
+
+    fig.update_xaxes(matches='x')
+
+    st.plotly_chart(fig)
+
+
+def simple_run(image_clean, image_cr, mask, mfill_smooth, mfill):
+
+
+    # Create subplots
+    fig = make_subplots(rows=1, cols=4,
+                        subplot_titles=("maskfill smooth", "maskfill"),
+                        specs=[[{}, None, {}, None]],
+                        shared_yaxes=True, shared_xaxes=True)
+
+    # Range
+    vmin,vmax=np.percentile(image_clean,[1,99])
+
+
+    trace1 = go.Heatmap(
+        z=mfill_smooth,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.44, title='Clean', thickness=15)
+    )
+    fig.add_trace(trace1, row=1, col=1)
+
+    trace2 = go.Heatmap(
+        z=mfill,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.96, title='Clean', thickness=15)
+    )
+    fig.add_trace(trace2, row=1, col=3)
+
+    # Update layout
+    fig.update_layout(
+        title_text='Output from simple maskfill run',
+        title_x=0.4,
+        title_y=1,
+        height=350,
+        margin=dict(l=0, r=0, b=40, t=45),
+        xaxis=dict(domain=[0, 0.45]),
+        xaxis2=dict(domain=[0.55, 0.97])
+    )
+
+    fig.update_xaxes(matches='x')
+
+    # Display the figure
+    st.plotly_chart(fig)
+
+
+def compare_image_simple_run(image_clean, image_cr, mask, mfill_smooth, mfill, vmin, vmax):
+
+    st.write("You can zoom into the region of image with the mouse selection.")
+
+        # Create subplots
+    fig = make_subplots(rows=1, cols=6,
+                        subplot_titles=("Clean", "maskfill", "maskfill smooth"),
+                        specs=[[{}, None, {}, None, {}, None]],
+                        shared_yaxes=True)
+
+
+    fig.add_trace(go.Heatmap(
+        z=image_clean,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.26, title='', thickness=15)), row=1, col=1
+    )
+
+    fig.add_trace(go.Heatmap(
+        z=mfill,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.61, title='', thickness=15)), row=1, col=3
+    )
+
+
+    fig.add_trace(go.Heatmap(
+        z=mfill_smooth,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.96, title='', thickness=15)), row=1, col=5
+    )
+
+    # Update layout
+    fig.update_layout(
+        title_text='Result From M51 Example',
+        title_x=0.4,
+        title_y=1,
+        height=250,
+        margin=dict(l=0, r=0, b=40, t=45),
+        xaxis=dict(domain=[0, 0.27]),
+        xaxis2=dict(domain=[0.35, 0.62]),
+        xaxis3=dict(domain=[0.7, 0.97])
+    )
+
+    fig.update_xaxes(matches='x')
+    fig.update_layout({"uirevision": "foo"}, overwrite=True)
+    st.plotly_chart(fig)
+
+
+
+def indices_within_distance(array, x, y, distance):
+    """
+    Creating an (arbitrary) large mask on a cutout from the m51 image.
+    https://maskfill.readthedocs.io/en/latest/python-usage.html#intermediate-outputs
+    """
+    rows, cols = np.indices(array.shape)
+    distances = np.sqrt((rows - x)**2 + (cols - y)**2)
+    indices = np.where(distances <= distance)
+    return indices
+
+
+@st.cache_data
+def create_cutout(image_clean):
+
+    image_cutout  = image_clean[390:490,400:500]
+    cutout_mask = np.zeros(image_cutout.shape)
+    cutout_mask[indices_within_distance(image_cutout,50,50,48)] = 1
+
+    # Convert the masked array to a regular NumPy array
+    masked_image = np.ma.masked_array(image_cutout, mask=cutout_mask.astype(bool)).filled(fill_value=np.nan)
+
+    return image_cutout, masked_image
+
+
+def see_image_cutout(image_cutout, masked_image):
+
+    #image_cutout  = image_clean[390:490,400:500]
+    #cutout_mask = np.zeros(image_cutout.shape)
+    #cutout_mask[indices_within_distance(image_cutout,50,50,48)] = 1
+
+    vmin,vmax=np.percentile(image_cutout,[1,99])
+
+
+
+
+    st.write("Examples of creating an (arbitrary) large mask on a cutout from the M51 image")
+
+        # Create subplots
+    fig = make_subplots(rows=1, cols=4,
+                        subplot_titles=("Cutout", "Mask"),
+                        specs=[[{}, None, {}, None]],
+                        shared_yaxes=True, shared_xaxes=True)
+
+
+    trace1 = go.Heatmap(
+        z=image_cutout,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.44, title='Cutout', thickness=15)
+    )
+    fig.add_trace(trace1, row=1, col=1)
+
+    trace2 = go.Heatmap(
+        z=masked_image,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.96, title='Cutout', thickness=15)
+    )
+    fig.add_trace(trace2, row=1, col=3)
+
+    # Update layout
+    fig.update_layout(
+        title_text='Masking Region',
+        title_x=0.4,
+        title_y=1,
+        height=350,
+        margin=dict(l=0, r=0, b=40, t=45),
+        xaxis=dict(domain=[0, 0.45]),
+        xaxis2=dict(domain=[0.55, 0.97])
+    )
+
+    fig.update_xaxes(matches='x')
+    fig.update_xaxes(showgrid=False)
+    fig.update_yaxes(showgrid=False)
+    fig.update_layout({"uirevision": "foo"}, overwrite=True)
+    # Display the figure
+    st.plotly_chart(fig)
+
+
+def one_kernel_size(image_clean, cutout_mask, fill_reg):
+
+    image_cutout  = image_clean[390:490,400:500]
+    cutout_mask = np.zeros(image_cutout.shape)
+    cutout_mask[indices_within_distance(image_cutout,50,50,48)] = 1
+    vmin,vmax=np.percentile(image_cutout,[1,99])
+
+    fill1, _ = maskfill(image_cutout,cutout_mask,size=fill_reg)
+
+
+        # Create subplots
+    fig = make_subplots(rows=1, cols=1,
+                        subplot_titles=(""),
+                        shared_yaxes=True)
+
+
+    fig.add_trace(go.Heatmap(
+        z=fill1,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.98, title='', thickness=15)), row=1, col=1
+    )
+
+
+    # Update layout
+    fig.update_layout(
+        title_text=f'Kernel Size {fill_reg}',
+        title_x=0.3,
+        title_y=1,
+        height=300,
+        width=300,
+        margin=dict(l=0, r=0, b=40, t=45),
+        xaxis=dict(domain=[0, 0.97]),
+    )
+
+    fig.update_xaxes(matches='x')
+    fig.update_layout({"uirevision": "foo"}, overwrite=True)
+    st.plotly_chart(fig)
+
+
+def kernel_size(image_clean, cutout_mask):
+
+    image_cutout  = image_clean[390:490,400:500]
+    cutout_mask = np.zeros(image_cutout.shape)
+    cutout_mask[indices_within_distance(image_cutout,50,50,48)] = 1
+    vmin,vmax=np.percentile(image_cutout,[1,99])
+
+    fill1, _ = maskfill(image_cutout,cutout_mask,size=3)
+    fill2, _ = maskfill(image_cutout,cutout_mask,size=5)
+    fill3, _ = maskfill(image_cutout,cutout_mask,size=11)
+
+
+        # Create subplots
+    fig = make_subplots(rows=1, cols=6,
+                        subplot_titles=("Kernel 3", "Kernel 5", "Kernel 11"),
+                        specs=[[{}, None, {}, None, {}, None]],
+                        shared_yaxes=True)
+
+
+    fig.add_trace(go.Heatmap(
+        z=fill1,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.26, title='', thickness=15)), row=1, col=1
+    )
+
+    fig.add_trace(go.Heatmap(
+        z=fill2,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.61, title='', thickness=15)), row=1, col=3
+    )
+
+
+    fig.add_trace(go.Heatmap(
+        z=fill3,
+        colorscale='Viridis',
+        zmin=vmin,
+        zmax=vmax,
+        colorbar=dict(x=0.96, title='', thickness=15)), row=1, col=5
+    )
+
+    # Update layout
+    fig.update_layout(
+        title_text='Kernel Size Examples for selected region in M51',
+        title_x=0.3,
+        title_y=1,
+        height=250,
+        margin=dict(l=0, r=0, b=40, t=45),
+        xaxis=dict(domain=[0, 0.27]),
+        xaxis2=dict(domain=[0.35, 0.62]),
+        xaxis3=dict(domain=[0.7, 0.97])
+    )
+
+    fig.update_xaxes(matches='x')
+    fig.update_layout({"uirevision": "foo"}, overwrite=True)
+    st.plotly_chart(fig)
---
+++ main.py
@@ -0,0 +1,174 @@
+import streamlit as st
+from maskfill import maskfill
+
+import numpy as np
+import matplotlib.pyplot as plt
+#import plotly.graph_objects as go
+#from plotly.subplots import make_subplots
+
+from helper_functions import import_m51_sample, run_mfill, show_data, simple_run, compare_image_simple_run,\
+								see_image_cutout, create_cutout, indices_within_distance, kernel_size,\
+								one_kernel_size
+
+
+
+image_clean, image_cr, mask = import_m51_sample()
+mfill_smooth, mfill = run_mfill(image_clean, image_cr, mask)
+
+# Range
+vmin_im, vmax_im = np.percentile(image_clean,[1,99])
+
+# Cast vmin_im and vmax_im to float
+vmin_im = float(vmin_im)
+vmax_im = float(vmax_im)
+
+
+with st.sidebar:
+	sel_options = st.radio("Select Options", ['See Sample', 'Compare Results [Simple Run]', 'Optional Arguments', 'References'])
+
+
+
+if sel_options == "See Sample":
+	st.header("Imported Sample")
+
+	st.write("Loading the uncorrupted and corrupted versions of the M51 example presented in [van Dokkum & Pasha (2024)](https://browse.arxiv.org/abs/2312.03064):")
+
+
+	show_data(image_clean, image_cr, mask)
+
+
+	st.subheader("Simple maskfill run:")
+	st.write("Results from an option-free run:")
+	simple_run(image_clean, image_cr, mask, mfill_smooth, mfill)
+
+
+
+if sel_options == "Compare Results [Simple Run]":
+	#compare_image_simple_run(image_clean, image_cr, mask, mfill_smooth, mfill, vmin_im, vmax_im)
+
+	#import plotly.figure_factory as ff
+
+
+	if "vmin" not in st.session_state:
+	    st.session_state["vmin"] = vmin_im
+	if "vmax" not in st.session_state:
+	    st.session_state["vmax"] = vmax_im
+
+	# put figure inside a form?
+	with st.form("form2"):
+		st.write("Click on the Button below to compare results on the plot. You can enhance fainter regions by selecting a smaller maximum value of 'Select range' and then click on the Button again to update plot.")
+		submitted2 = st.form_submit_button(label="Show Plot and Update")
+		vals = st.slider("Select range", vmin_im, vmax_im, (vmin_im, vmax_im))
+
+		new_vmin = vals[0] #st.slider("Min", min_value=vmin_im, max_value=vmax_im, value=vmin_im, step=0.1)
+		new_vmax = vals[1] #st.slider("Max", min_value=vmin_im, max_value=vmax_im, value=vmax_im, step=0.1)
+
+	if submitted2:
+		#mfill_smooth, mfill = maskfill(image_cr, mask)
+		
+		compare_image_simple_run(image_clean, image_cr, mask, mfill_smooth, mfill, new_vmin, new_vmax)
+	            # Create subplots
+
+if sel_options == "Optional Arguments":
+	with st.sidebar:
+		arg_options = st.radio("Arguments", ["Window Size"])
+
+		
+	if arg_options == "Window Size":
+
+		st.header("See impact of window size selection")
+		st.write('" This argument controls the window of adjacent pixels used for computation of the local medians, and for the final smoothing step" [van Dokkum & Pasha (2024)](https://maskfill.readthedocs.io/en/latest/python-usage.html#intermediate-outputs)')
+
+		image_cutout, masked_image = create_cutout(image_clean)
+		see_image_cutout(image_cutout, masked_image)
+
+		
+		ker_option = st.radio("Choose option", ["Single Kernel Size", "Multiple Kernels Comparison"])
+
+		if ker_option == "Single Kernel Size":
+
+			fill_reg = st.slider("Kernel Size", min_value=3, max_value=15, value=3, step=2)
+			one_kernel_size(image_clean, masked_image, fill_reg)
+
+		if ker_option == "Multiple Kernels Comparison":
+			
+		
+			kernel_size(image_clean, masked_image)
+
+if sel_options == "References":
+	st.header("References")
+	st.write(
+		"""Python package Maskfill is made by Pieter van Dokkum & Imad Pasha, see [van Dokkum & Pasha (2024)](https://browse.arxiv.org/abs/2312.03064)
+	""")
+	st.write("")
+	st.write(""" 
+		Check Maskfill [**documentation**](https://maskfill.readthedocs.io/en/latest/index.html).
+		""")
+	st.write("")
+	st.write(""" 
+		Maskfill code is available on [**GitHub**](https://github.com/dokkum/maskfill).
+		""")
+
+	st.write(""" 
+		This Streamlit application is made by Robert Dzudzar, code of the app can be found on [**GitHub**](https://github.com/rdzudzar).
+		""")
+	st.write("")
+	st.write(""" 
+	If you find maskfill useful in your research or image handlings, please cite the code:
+
+	@ARTICLE{2023arXiv231203064V,
+       author = {{van Dokkum}, Pieter and {Pasha}, Imad},
+        title = "{A robust and simple method for filling in masked data in astronomical images}",
+      journal = {arXiv e-prints},
+     keywords = {Astrophysics - Instrumentation and Methods for Astrophysics, Astrophysics - Astrophysics of Galaxies},
+         year = 2023,
+        month = dec,
+          eid = {arXiv:2312.03064},
+        pages = {arXiv:2312.03064},
+          doi = {10.48550/arXiv.2312.03064},
+	archivePrefix = {arXiv},
+       eprint = {2312.03064},
+	 primaryClass = {astro-ph.IM},
+       adsurl = {https://ui.adsabs.harvard.edu/abs/2023arXiv231203064V},
+      adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+	}
+			""")
+
+
+with st.sidebar:
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	
+	st.sidebar.info(
+		"""This Streamlit app showcase examples from **maskfill** Python package.
+		Original [**maskfill**](https://maskfill.readthedocs.io/en/latest/index.html) Python package 
+		is made by Pieter van Dokkum & Imad Pasha, see [van Dokkum & Pasha (2024)](https://browse.arxiv.org/abs/2312.03064)
+		"""
+		)
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+	st.write("")
+
+	col1, col2 = st.columns([0.7,0.2])
+	with col1:
+
+		st.markdown('''
+	    <a href="https://scistreams.github.io">
+	        <img src="https://scistreams.github.io/images/SciStreams.png" width="150" />
+	    </a>''',
+	    unsafe_allow_html=True
+		)
+		st.markdown('App made by [**SciStreams**](https://scistreams.github.io/)')
